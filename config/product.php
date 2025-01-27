864012c960378ebce03cbf9e4fa10e1366c0eb14
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2025-01-27 17:07:36 +0600

    Some Modified

---
+++ config/product.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'licenseCodeCheckerUrl' => 'https://support.mhl.net',
+    'officialSite'          => 'https://mhl.net',
+    'loginUrl'              => 'https://mhl.net/login',
+    'activeLicense'         => 'https://mhl.net/active-license',
+    'itemId'                => '50280367',
+    'version'               => '1.7'
+];
